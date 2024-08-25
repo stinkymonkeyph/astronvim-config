7481e13f5f7a68610557562e7113a210e26d14b7
stinkymonkeyph <monkeydev.team@gmail.com>
2024-08-25 15:35:34 +0800

    feat: add typescript language pack

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
 }
